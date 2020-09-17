d2bdee04c2e32d366b9d10ddd0347ad829887e24
Deividas <deizyra@hotmail.lt>
2020-09-17 12:36:08 +0300

    Antras pushas

--- src/User.php
+++ src/User.php
@@ -6,6 +6,11 @@ class User
     public $email;
     public $role;
 
+    public function __construct($name, $email, $role){
+        $this->name = $name;
+        $this->email = $email;
+        $this->role = $role;
+    }
     public function showProfile(){
         $data[] = $this->name;
         $data[] = $this->email;
--- view/index.view.php
+++ view/index.view.php
@@ -2,7 +2,7 @@
 
 use userApp\User;
 
-$user = new User();
+$user = new User('Petras', 'petras@gmail.com', 'admin');
 $user->name = "Petras";
 $user->email = "petras@gmail.com";
 $user->role = "admin";
